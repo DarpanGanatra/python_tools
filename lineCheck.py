1259a42c65b5a9d37b5482ccbd3e1226f4fc6931
Darpan Ganatra <darpan@Darpans-MacBook-Pro.local>
2017-10-01 03:10:29 -0400

    Finished lineCheck

--- lineCheck.py
+++ lineCheck.py
@@ -3,3 +3,27 @@
 import os 
 import argparse
 from os.path import join
+
+
+argparser = argparse.ArgumentParser (description 'Gettin started')
+argparser.add_argument('--input', default =".", type=str, help="Tell me the path to the file")
+argparser.add_argument('--len', type=int, help="Tell me the length of the string you want to find")
+
+args = argparser.parse_args()
+place = args.input
+length = args.len
+
+def find(place, length):
+	newlist =[]
+	for root, dirs, files in os.walk(place):
+            for j in files:
+                if j in files:
+                    if j.endswith(".txt"):
+                        newlist.append(join(root,j))
+                    for i in newlist:
+                        with open(i) as fi:
+                            for count,line in enumerate(fi, 1):
+                                if len(line) > length:
+                                    print(len(line),count,i)
+find(place,length)
+
