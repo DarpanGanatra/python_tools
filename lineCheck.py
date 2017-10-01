a836b3b8aec2ba6cbd1d794a66ed79da8d96e093
Darpan Ganatra <darpan@Darpans-MacBook-Pro.local>
2017-10-01 02:33:30 -0400

    Gettin' started

---
+++ lineCheck.py
@@ -0,0 +1,5 @@
+'''This program will allow you to go through a directory and it's subdirectories in order to figure out if there exists a line of a particular length '''
+
+import os 
+import argparse
+from os.path import join
